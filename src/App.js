905f3fe0e4f3075b40c31b660ee0e3360d72e25a
bk-tripathi <balkrishna.tri09@gmail.com>
2023-08-06 17:11:12 +0530

    outputting cart & connect Backend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { Fragment } from "react";
+import {  useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
+  const [CartIsShown, setCartIsShown] =useState();
+
+  const showCartHandler =() =>{
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler=() =>{
+    setCartIsShown(false);
+  }
+
   return (
-    <Fragment>
-      <Cart />
-      <Header />
+    <CartProvider>
+      {CartIsShown && <Cart  onHideCart={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
